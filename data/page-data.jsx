9cd3171b8659ca635296a4438a38872075c4b900
jamilurrahmanmuhammad <jamil.muhammad@gmail.com>
2023-06-03 11:44:22 +0300

    All commit

---
+++ data/page-data.jsx
@@ -0,0 +1,74 @@
+export const personalData = {
+	name: "JamilUrRahman Muhammad",
+	role: "Solution Architect",
+	education: [
+		"BS in Computer Engineering, Sir Syed University Of Engineering And Technology(1999-2002)"
+	],
+	contactLinks: [
+		"mailto:emailto:jamil.muhammad@gmail.com",
+		"https://twitter.com/jamil_urahman",
+		"www.linkedin.com/in/1jamilurrahman",
+		"https://medium.com/@jamil.muhammad",
+	],
+};
+
+export const aboutMe = {
+	title: "About Me",
+	body: [
+		"Jamil possesses an experience of more than 20 years in Software design and leadership roles across industries spanning Telecommunication, Banking, Oil & Gas, Software house and Education.",
+
+		"As a Solution Architect, he has extensively participated in integration and solution designing for Cloud Native Computing, Microservices, Containerization, Open API, API Management, Product Catalogue Management, Promotional Offering, Order Management, B2B with Partners and Master Data Management.",
+		
+		"Achievements:",
+		
+		"Certificate of appreciation from Master Works",
+		"Certificate of appreciation from Saudi Telecom Company",
+		"Certificate of appreciation from Mobily",
+		
+	],
+};
+
+export const skills = {
+	soft: [
+		{ icon: "👂🏼", text: "Active Listening" },
+		{ icon: "💬", text: "Effective Communication" },
+		{ icon: "👥", text: "Collaboration" },
+		{ icon: "⽓", text: "Teamwork" },
+		{ icon: "💡", text: "Creative Problem Solving" },
+		{ icon: "⌛️", text: "Time management" },
+	],
+	hard: [
+		{ icon: "💻", text: "Solutioning" },
+		{ icon: "ʦ", text: "Architecting" },
+		{ icon: "🚀", text: "Object Oriented Analysis & Design" },
+		{ icon: "💾", text: "Java" },
+		{ icon: "💾", text: "SQL" },
+		{ icon: "📈", text: "Data Structures and Algorithms" },
+	],
+};
+
+export const professionalData = {
+	title: "Professional Experience",
+	experiences: [
+		{
+			role: "Solution Architect / Scrum Master, Masterworks (2022-present)",
+			description:
+				"Solution And Design cloud native application, integration services and Open APIs.",
+			current: true,
+		},
+
+		{
+			role: "Integration Architect / Scrum Master, stc via Ejada. (2019-2022)",
+			description:
+				"Designed and implemented integration services for both BSS and OSS systems of telecom",
+			current: false,
+		},
+		{
+			role: "Principal Integration Specialist, Mobily. (2007-2019)",
+			description:
+				
+				"Added value to the organization by coming up with framework to enable launching promotional offers in a highly reduced TTM.",
+			current: false,
+		},
+	],
+};
